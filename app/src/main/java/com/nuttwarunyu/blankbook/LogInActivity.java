4dc07fdedf47821e9bac698294a62cbf3728355d
Gwarunyu <gwarunyu@gmail.com>
2016-01-08 18:57:48 +0700

    FB Login Save

--- app/src/main/java/com/nuttwarunyu/blankbook/LogInActivity.java
+++ app/src/main/java/com/nuttwarunyu/blankbook/LogInActivity.java
@@ -13,15 +13,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.login.widget.LoginButton;
 import com.parse.LogInCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.Permission;
 import java.security.Signature;
+import java.util.Collection;
 
 
 public class LogInActivity extends AppCompatActivity {
@@ -29,6 +40,7 @@ public class LogInActivity extends AppCompatActivity {
     Button loginBtn;
     EditText loginUsername, loginPassword;
     TextView loginRegister;
+    Permission permission;
 
     private void bindWidget() {
         loginBtn = (Button) findViewById(R.id.login_button);
@@ -45,23 +57,13 @@ public class LogInActivity extends AppCompatActivity {
 
         bindWidget();
 
-        /*try {
-            PackageInfo info = getPackageManager().getPackageInfo("com.nuttwarunyu.blankbook", PackageManager.GET_SIGNATURES);
-            for (android.content.pm.Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+        LoginButton fbLoginButton = (LoginButton) findViewById(R.id.facebookLoginButton);
+        fbLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFacebookLogIn();
             }
-
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-*/
-
-//        LoginButton fbLoginButton = (LoginButton)findViewById(R.id.facebookLoginButton);
-
+        });
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,5 +101,25 @@ public class LogInActivity extends AppCompatActivity {
         });
     }
 
+    private void openFacebookLogIn() {
+        ParseFacebookUtils.logInWithReadPermissionsInBackground(LogInActivity.this, (Collection<String>) permission, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (user == null) {
+                    Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
+                } else if (user.isNew()) {
+                    Log.d("MyApp", "User signed up and logged in through Facebook!");
+                } else {
+                    Log.d("MyApp", "User logged in through Facebook!");
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+    }
 }
 
