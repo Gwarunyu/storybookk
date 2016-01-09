1442a97a4c95324c7ef4349586dfea5c4f414b3a
Gwarunyu <gwarunyu@gmail.com>
2016-01-09 21:59:06 +0700

    FB Login Save

--- app/src/main/java/com/nuttwarunyu/blankbook/LogInActivity.java
+++ app/src/main/java/com/nuttwarunyu/blankbook/LogInActivity.java
@@ -3,6 +3,10 @@ package com.nuttwarunyu.blankbook;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -10,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,11 +28,19 @@ import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.Permission;
@@ -41,12 +54,16 @@ public class LogInActivity extends AppCompatActivity {
     EditText loginUsername, loginPassword;
     TextView loginRegister;
     Permission permission;
+    ParseUser parseUser;
+    ImageView mProfileImage;
+    String email, name;
 
     private void bindWidget() {
         loginBtn = (Button) findViewById(R.id.login_button);
         loginPassword = (EditText) findViewById(R.id.login_password);
         loginUsername = (EditText) findViewById(R.id.login_username);
         loginRegister = (TextView) findViewById(R.id.login_register);
+        mProfileImage = (ImageView) findViewById(R.id.mProfileImage);
     }
 
     @Override
@@ -105,12 +122,17 @@ public class LogInActivity extends AppCompatActivity {
         ParseFacebookUtils.logInWithReadPermissionsInBackground(LogInActivity.this, (Collection<String>) permission, new LogInCallback() {
             @Override
             public void done(ParseUser user, ParseException e) {
+                Log.d("openFacebookLogIn()", " User's Value  is : " + user);
                 if (user == null) {
                     Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
                 } else if (user.isNew()) {
                     Log.d("MyApp", "User signed up and logged in through Facebook!");
+                    getUserDetailsFromFB();
                 } else {
                     Log.d("MyApp", "User logged in through Facebook!");
+                    getUserDetailsFromFB();
+
+                    //getUserDetailsFromParse();
                 }
             }
         });
@@ -121,5 +143,124 @@ public class LogInActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
     }
+
+    private void getUserDetailsFromFB() {
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "email,name,picture");
+
+        new GraphRequest(AccessToken.getCurrentAccessToken(), "/me", parameters, HttpMethod.GET, new GraphRequest.Callback() {
+            @Override
+            public void onCompleted(GraphResponse response) {
+                try {
+//TODO                    email = response.getJSONObject().getString("email");
+                    name = response.getJSONObject().getString("name");
+
+                    Log.d("GraphRequest ", " is response.... Name :" + name + "  email :" + email);
+
+                    JSONObject picture = response.getJSONObject().getJSONObject("picture");
+                    Log.d("GraphRequest", " getPicture is : " + picture);
+                    JSONObject data = picture.getJSONObject("data");
+
+                    //  Returns a 50x50 profile picture
+                    String pictureUrl = data.getString("url");
+                    new ProfilePhotoAsync(pictureUrl).execute();
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).executeAsync();
+    }
+
+    class ProfilePhotoAsync extends AsyncTask<String, String, String> {
+        public Bitmap bitmap;
+        String url;
+
+        public ProfilePhotoAsync(String url) {
+            this.url = url;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            // Fetching data from URI and storing in bitmap
+            bitmap = DownloadImageBitmap(url);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            mProfileImage.setImageBitmap(bitmap);
+            saveNewUser();
+        }
+    }
+
+    private void saveNewUser() {
+        parseUser = ParseUser.getCurrentUser();
+        parseUser.setUsername(name);
+        //TODO  parseUser.setEmail(email);
+
+        Log.d("saveNewUser()", "Process");
+//        Saving profile photo as a ParseFile
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        Bitmap bitmap = ((BitmapDrawable) mProfileImage.getDrawable()).getBitmap();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+        byte[] data = stream.toByteArray();
+        String thumbName = parseUser.getUsername().replaceAll("\\s+", "");
+        final ParseFile parseFile = new ParseFile(thumbName + "_thumb.jpg", data);
+        parseFile.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                parseUser.put("profileThumb", parseFile);
+                //Finally save all the user details
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Toast.makeText(getApplicationContext(), "New user:" + name + " Signed up", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    public static Bitmap DownloadImageBitmap(String url) {
+        Bitmap bm = null;
+        try {
+            Log.d("DownloadImageBitmap", "Process");
+            URL aURL = new URL(url);
+            URLConnection conn = aURL.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bm = BitmapFactory.decodeStream(bis);
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+            Log.e("IMAGE", "Error getting bitmap", e);
+        }
+        return bm;
+    }
+
+    private void getUserDetailsFromParse() {
+        parseUser = ParseUser.getCurrentUser();
+
+        //Fetch profile photo
+        try {
+            ParseFile file = parseUser.getParseFile("profileThumb");
+            byte[] data = file.getData();
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            mProfileImage.setImageBitmap(bitmap);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        loginRegister.setText(parseUser.getUsername());
+
+        Toast.makeText(LogInActivity.this, "Welcome Back  " + loginRegister.getText().toString(), Toast.LENGTH_SHORT).show();
+
+    }
+
 }
 
