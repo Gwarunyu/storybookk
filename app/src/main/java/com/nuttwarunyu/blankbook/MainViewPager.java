9991e54b5f4820430ff3a59cff9ab664e37492bf
Gwarunyu <gwarunyu@gmail.com>
2016-01-10 23:53:50 +0700

    actionBar

--- app/src/main/java/com/nuttwarunyu/blankbook/MainViewPager.java
+++ app/src/main/java/com/nuttwarunyu/blankbook/MainViewPager.java
@@ -7,7 +7,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
 import com.facebook.AccessToken;
@@ -48,5 +50,21 @@ public class MainViewPager extends AppCompatActivity {
         Log.d("TAG MENU : ", "  " + menu);
         getMenuInflater().inflate(R.menu.main_action, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                Toast.makeText(MainViewPager.this, "case R.id.action_logout:", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_profile:
+                Toast.makeText(MainViewPager.this, "case R.id.action_profile:", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
